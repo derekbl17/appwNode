5e35b60de86ba3c0e2a57001082f6b46d39a4b25
derekbl17 <derekc1fill@gmail.com>
2024-12-19 13:39:57 +0200

    ss

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const form = document.getElementById("tree-form");
-
+const table = document.getElementById("myTable");
+const createButton = document.getElementById("create");
+const updateButton = document.getElementById("updateSave");
+const nameInput = document.getElementById("create-name");
+const heightInput = document.getElementById("create-height");
+const typeInput = document.getElementById("create-type");
 //@ POST /trees
 form.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -22,6 +27,93 @@ form.addEventListener("submit", (event) => {
 function fetchTrees() {
   fetch("http://localhost:3000/trees")
     .then((response) => response.json())
-    .then((data) => {});
+    .then((data) => {
+      const tableBody = document.getElementById("tableBody");
+      tableBody.innerHTML = "";
+      if (data.length === 0) {
+        document.getElementById(
+          "messages"
+        ).innerHTML = `<h2>No data found</h2>`;
+        table.style.display = "none";
+      } else {
+        data.forEach((el, index) => {
+          let row = document.createElement("tr");
+          row.id = `row-${el.id}`;
+          row.innerHTML = `
+          <td>${index + 1}</td>
+          <td>${el.name}</td>
+          <td>${el.height}</td>
+          <td>${el.type}</td>
+          <td>
+          <button class="edit" data-id="${el.id}">EDIT</button>
+          <button class="delete" data-id="${el.id}">DELETE</button>
+          </td>`;
+          tableBody.appendChild(row);
+        });
+      }
+    });
 }
+
+//@ DELETE /trees/:id
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete")) {
+    const treeID = e.target.getAttribute("data-id");
+    console.log(treeID);
+    fetch("http://localhost:3000/trees/" + treeID, { method: "DELETE" })
+      .then((res) => res.json())
+      .then(() => {
+        fetchTrees();
+        window.location.href = "/index.html";
+        // updateButton.style.display = "none";
+        // createButton.style.display = "inline-block";
+      });
+  }
+});
+
+//@ GET /trees/:id
+document.addEventListener("click", (e) => {
+  if (e.target.classList.contains("edit")) {
+    const treeID = e.target.getAttribute("data-id");
+    console.log(treeID);
+    window.history.pushState(null, "", `/?treeID=${treeID}`);
+    fetch("http://localhost:3000/trees/" + treeID)
+      .then((res) => res.json())
+      .then((data) => {
+        nameInput.value = data.name;
+        heightInput.value = data.height;
+        typeInput.value = data.type;
+        createButton.style.display = "none";
+        updateButton.style.display = "inline-block";
+        updateButton.setAttribute("data-id", treeID);
+
+        const deletebtn = document.querySelectorAll(".delete");
+        console.log(deletebtn);
+        deletebtn.forEach((el) => el.setAttribute("disabled", true));
+      });
+  }
+});
+
+// @PUT
+updateButton.addEventListener("click", (e) => {
+  const treeID = e.target.getAttribute("data-id");
+  const updatedData = {
+    name: nameInput.value,
+    height: heightInput.value,
+    type: typeInput.value,
+  };
+  console.log(updatedData);
+  fetch(`http://localhost:3000/trees/${treeID}`, {
+    method: "PUT",
+    headers: { "Content-type": "application/json" },
+    body: JSON.stringify(updatedData),
+  })
+    .then((res) => res.json())
+    .then(() => {
+      updateButton.style.display = "none";
+      createButton.style.display = "inline-block";
+      form.reset();
+      window.location.href = "/index.html";
+    });
+});
+
 fetchTrees();
